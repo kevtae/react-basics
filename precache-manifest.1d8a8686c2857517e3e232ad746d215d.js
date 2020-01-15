16504e8f01f6316459e63f73075ec8c603db7581
Kevtae <taelim314@gmail.com>
2020-01-15 18:11:01 -0500

    Updates

---
+++ precache-manifest.1d8a8686c2857517e3e232ad746d215d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c365662534f7af91b5cf7578b1b23c39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e3ef7b8c5abf75e6a5a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "706f41ffbdf26bf106bf",
+    "url": "/static/js/2.39513104.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.39513104.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e3ef7b8c5abf75e6a5a",
+    "url": "/static/js/main.6420751c.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
